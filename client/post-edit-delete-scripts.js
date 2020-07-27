e78197e3662196fb26166633231199b113cbf1ef
Isaiah Murray <murray.isaiah13@gmail.com>
2020-07-27 10:42:04 -0400

    Added delete method

--- client/post-edit-delete-scripts.js
+++ client/post-edit-delete-scripts.js
@@ -30,7 +30,36 @@ function postJournal() {
      *** UPDATE JOURNAL ***
     ************************** */
     function editJournal(postId) {
+        console.log(postId)
+        const fetch_url = `http://localhost:3000/journal/update/${postId}`
+        const accessToken = localStorage.getItem('SessionToken')
         
+
+        let card = document.getElementById(postId)
+        let input = document.createElement('input')
+
+        if(card.childNodes.length < 2){
+            card.appendChild(input)
+            input.setAttribute('type', 'text')
+            input.setAttribute('id', 'updatedEntry')
+            input.setAttribute('placeholder', 'Edit your journal entry')
+        } else {
+            let updateId = document.getElementById('updatedEntry').value
+            let updatedEntry = { journal: { entry: updateId } };
+            const response = fetch(fetch_url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': accessToken
+                },
+                body: JSON.stringify(updatedEntry)
+            })
+            .then(data => {
+                console.log(data)
+                displayMine();
+            })
+            card.removeChild(card.lastChild)
+        }
     }
     
     
@@ -38,5 +67,21 @@ function postJournal() {
      *** DELETE JOURNAL ***
     ************************** */
     function deleteJournal(postId) {
-     console.log('deleteJournal Function Called')
+     console.log('deleteJournal working')
+    console.log(postId)
+
+    const fetch_url = `http://localhost:3000/journal/delete/${postId}`
+    const accessToken = localStorage.getItem('SessionToken')
+
+    fetch(fetch_url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        }
+    })
+    .then(response => {
+        console.log(response);
+        displayMine();
+    })
     }
