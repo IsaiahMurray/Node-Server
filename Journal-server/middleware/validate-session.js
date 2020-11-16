ba8217c49ea019fd1a9844fef4f73176437c1dd1
Isaiah Murray <murray.isaiah13@gmail.com>
2020-11-16 16:24:47 -0500

    Pulled from git

---
+++ Journal-server/middleware/validate-session.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('../db').import('../models/user');
+
+const validateSession = (req, res, next) => {
+    const token = req.headers.authorization;
+    console.log('token -->', token);
+    if(!token){
+        return res.status(403).send({auth: false, message: "No token provided"})
+    } else {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+            console.log('decodeToken -->', decodeToken);
+            if(!err && decoded){
+                User.findOne({
+                    where: {
+                        id: decodedToken.id
+                    }
+                })
+                .then(user => {
+                    console.log('user -->', user);
+                    if(!user) throw err;
+                    console.log('req -->', req);
+                    req.user;
+                    return next();
+                })
+                .catch(err => next(err));
+            } else{
+                req.errors = err;
+                return res.status(500).send('Not Authorized');
+            }
+        });
+    }
+};
+
+module.exports = validateSession;
